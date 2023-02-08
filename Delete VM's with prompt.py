4bbd1505c0b6ba098bf8bdda123a6b30845a201c
LostEpic <10285730+Lostepic@users.noreply.github.com>
2023-02-08 08:39:10 +0000

    Updated Delete VM script fixed errors

--- Delete VM's with prompt.py
+++ Delete VM's with prompt.py
@@ -1,26 +1,48 @@
-import linode_api4
-import os
+import requests
+import json
 
-# Initialize the API client
-api_key = os.getenv("LINODE_API_KEY")
-client = linode_api4.Client(auth_token=api_key)
+# API key for authentication
+api_key = "PUT-API-KEY-HERE"
 
-# List all Linodes associated with your account
-linodes = client.linode.list()
+# API endpoint for getting a list of instances
+instances_endpoint = "https://api.linode.com/v4/linode/instances"
 
-# Print a numbered list of the Linodes
-for i, linode in enumerate(linodes):
-    print(f"{i+1}. {linode.label} (ID: {linode.id})")
+# API endpoint for deleting an instance
+delete_instance_endpoint = "https://api.linode.com/v4/linode/instances/{}"
 
-# Prompt the user to select a Linode for deletion
-selected_index = int(input("Enter the number of the Linode you wish to delete: ")) - 1
-selected_linode = linodes[selected_index]
+# Headers for API request
+headers = {
+    "Authorization": "Bearer {}".format(api_key),
+    "Content-Type": "application/json"
+}
 
-# Confirm the deletion with the user
-confirmation = input(f"Are you sure you want to delete the Linode '{selected_linode.label}' (ID: {selected_linode.id})? [y/N] ")
-if confirmation.lower() == "y":
-    # Delete the selected Linode
-    client.linode.delete(selected_linode.id)
-    print(f"Linode '{selected_linode.label}' (ID: {selected_linode.id}) has been deleted.")
+# Get a list of instances
+response = requests.get(instances_endpoint, headers=headers)
+
+# Check if the request was successful
+if response.status_code == 200:
+    instances = response.json()["data"]
+    
+    # Print the list of instances and ask the user to select one for deletion
+    for i, instance in enumerate(instances):
+        print("[{}] {} ({})".format(i+1, instance["label"], instance["id"]))
+    
+    selected_instance = int(input("Enter the number of the instance you want to delete: ")) - 1
+    instance_id = instances[selected_instance]["id"]
+    
+    # Confirm with the user if they really want to delete the selected instance
+    confirm = input("Are you sure you want to delete instance '{}' (yes/no)? ".format(instances[selected_instance]["label"]))
+    
+    # Delete the selected instance if the user confirms
+    if confirm.lower() == "yes":
+        response = requests.delete(delete_instance_endpoint.format(instance_id), headers=headers)
+        
+        # Check if the request was successful
+        if response.status_code == 200:
+            print("Instance successfully deleted.")
+        else:
+            print("Failed to delete instance. Error: {}".format(response.text))
+    else:
+        print("Deletion cancelled.")
 else:
-    print("Deletion cancelled.")
+    print("Failed to get a list of instances. Error: {}".format(response.text))
